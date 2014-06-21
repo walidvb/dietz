1b9b2448916a15d9922e6a31c0ad71f834066d18
Walid <walidvb@gmail.com>
2014-06-21 15:07:34 +0200

    implements mousewheel nav

--- all/themes/clean/js/superframe.js
+++ all/themes/clean/js/superframe.js
@@ -7,6 +7,7 @@ var mobile = "";
 var superTimeOut;
 var timeOut;
 var initialPageLoad = true;
+var lastTime = new Date();
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 	mobile = "-mobile";
 }
@@ -68,17 +69,20 @@ jQuery.fn.replace = function()
 		//previous project
 		up_ = function(e) {
 			e.preventDefault();
+			lastTime = new Date();
 			$('body').addClass('in-transit');
 			superframe.cycle('next');
 		}
 		//next project
 		down_ = function(e) {
 			e.preventDefault();
+			lastTime = new Date();
 			$('body').addClass('in-transit');
 			superframe.cycle('prev');
 		}
 		//previous frame
 		left_ = function(e) {
+			lastTime = new Date();
 			//console.log(currMiniframe);
 			if(currMiniframe == 0 && currFrame.parents('.miniframe').hasClass('closed'))
 			{
@@ -91,6 +95,7 @@ jQuery.fn.replace = function()
 		}
 		//next frame
 		right_ = function(e) {
+			lastTime = new Date();
 			if(currMiniframe == 0 && !currFrame.parents('.miniframe').hasClass('closed'))
 			{
 				toggleInfo();
@@ -109,7 +114,6 @@ jQuery.fn.replace = function()
     	//load further images
     	$(document).bind('cycle-before', function(event, optionHash, outgoingSlideEl, incoming, ff){
 		    //cancel whatever had been scheduled
-		    
 		    //if this is superframe
 		    if(event.target.className == "view-content")
 		    {
@@ -169,6 +173,7 @@ jQuery.fn.replace = function()
 
 $(document).bind('cycle-after', function(event, optionHash, outgoingSlideEl, incoming, forwardFlag)		{
 			//if this is superframe
+
 			if(event.target.className == "view-content")
 			{	
 				$('body').removeClass('in-transit');
@@ -254,7 +259,7 @@ $(document).bind('cycle-after', function(event, optionHash, outgoingSlideEl, inc
 		}		
 		nav.add(info).appendTo($('body'));
 
-
+		var interval;
 		//miniframes
 		var opts = {
 			timeout: 0,
@@ -348,9 +353,44 @@ $(document).bind('cycle-after', function(event, optionHash, outgoingSlideEl, inc
     info.once('sfed', function(){$(this).on('click.nav', toggleInfo)});
     Mousetrap.bind('i', toggleInfo);
 
+    //bind mousewheel
+    superframe.on('mousewheel', wheelMove);
+
+    function wheelMove(e, deltaY) {
+    	//superframe.unbind('mousewheel', wheelMove);
+    	var threshold = 0;
+    	if(new Date() - lastTime > 1000)
+    	{
+    		lastTime = new Date();
+    		if (e.deltaY > threshold) {
+    			up_(e);
+    			return;
+    		}
+    		else if (e.deltaY < -threshold) {
+    			down_(e);
+    			return;
+    		}
+    		if(e.deltaX < threshold){
+    			left_(e);
+    		}
+    		else if(e.deltaX > threshold){
+    			right_(e);
+    		}
+    	}
+    }
 
 	//EOBEHAVIOR	
 }
 };
 
 })(jQuery);
+
+//include mousewheel
+/*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Version: 3.1.11
+ *
+ * Requires: jQuery 1.2.2+
+ */
+ !function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a:a(jQuery)}(function(a){function b(b){var g=b||window.event,h=i.call(arguments,1),j=0,l=0,m=0,n=0,o=0,p=0;if(b=a.event.fix(g),b.type="mousewheel","detail"in g&&(m=-1*g.detail),"wheelDelta"in g&&(m=g.wheelDelta),"wheelDeltaY"in g&&(m=g.wheelDeltaY),"wheelDeltaX"in g&&(l=-1*g.wheelDeltaX),"axis"in g&&g.axis===g.HORIZONTAL_AXIS&&(l=-1*m,m=0),j=0===m?l:m,"deltaY"in g&&(m=-1*g.deltaY,j=m),"deltaX"in g&&(l=g.deltaX,0===m&&(j=-1*l)),0!==m||0!==l){if(1===g.deltaMode){var q=a.data(this,"mousewheel-line-height");j*=q,m*=q,l*=q}else if(2===g.deltaMode){var r=a.data(this,"mousewheel-page-height");j*=r,m*=r,l*=r}if(n=Math.max(Math.abs(m),Math.abs(l)),(!f||f>n)&&(f=n,d(g,n)&&(f/=40)),d(g,n)&&(j/=40,l/=40,m/=40),j=Math[j>=1?"floor":"ceil"](j/f),l=Math[l>=1?"floor":"ceil"](l/f),m=Math[m>=1?"floor":"ceil"](m/f),k.settings.normalizeOffset&&this.getBoundingClientRect){var s=this.getBoundingClientRect();o=b.clientX-s.left,p=b.clientY-s.top}return b.deltaX=l,b.deltaY=m,b.deltaFactor=f,b.offsetX=o,b.offsetY=p,b.deltaMode=0,h.unshift(b,j,l,m),e&&clearTimeout(e),e=setTimeout(c,200),(a.event.dispatch||a.event.handle).apply(this,h)}}function c(){f=null}function d(a,b){return k.settings.adjustOldDeltas&&"mousewheel"===a.type&&b%120===0}var e,f,g=["wheel","mousewheel","DOMMouseScroll","MozMousePixelScroll"],h="onwheel"in document||document.documentMode>=9?["wheel"]:["mousewheel","DomMouseScroll","MozMousePixelScroll"],i=Array.prototype.slice;if(a.event.fixHooks)for(var j=g.length;j;)a.event.fixHooks[g[--j]]=a.event.mouseHooks;var k=a.event.special.mousewheel={version:"3.1.11",setup:function(){if(this.addEventListener)for(var c=h.length;c;)this.addEventListener(h[--c],b,!1);else this.onmousewheel=b;a.data(this,"mousewheel-line-height",k.getLineHeight(this)),a.data(this,"mousewheel-page-height",k.getPageHeight(this))},teardown:function(){if(this.removeEventListener)for(var c=h.length;c;)this.removeEventListener(h[--c],b,!1);else this.onmousewheel=null;a.removeData(this,"mousewheel-line-height"),a.removeData(this,"mousewheel-page-height")},getLineHeight:function(b){var c=a(b)["offsetParent"in a.fn?"offsetParent":"parent"]();return c.length||(c=a("body")),parseInt(c.css("fontSize"),10)},getPageHeight:function(b){return a(b).height()},settings:{adjustOldDeltas:!0,normalizeOffset:!0}};a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})});
